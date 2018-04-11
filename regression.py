9847e671f7915c093b557853c8a2fc6cd7acecb2
h908714124 <kraftdurchblumen@gmx.de>
2018-04-11 10:01:32 +0200

    initial commit

---
+++ regression.py
@@ -0,0 +1,72 @@
+'''
+A linear regression learning algorithm example using TensorFlow library.
+Author: Aymeric Damien
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+from __future__ import print_function
+
+import matplotlib.pyplot as plt
+import numpy
+import tensorflow as tf
+
+rng = numpy.random
+
+# Parameters
+learning_rate = 0.0001
+training_epochs = 1000
+display_step = 50
+
+# Training Data
+train_X = numpy.asarray([3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167,
+                         7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1])
+train_Y = numpy.asarray([1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221,
+                         2.827, 3.465, 1.65, 2.904, 2.42, 2.94, 1.3])
+
+n_samples = train_X.shape[0]
+
+# tf Graph Input
+X = tf.placeholder("float")
+Y = tf.placeholder("float")
+
+# Set model weights
+m = tf.Variable(rng.randn(), name="weight")
+b = tf.Variable(rng.randn(), name="bias")
+
+# Construct a linear model
+pred = tf.add(tf.multiply(X, m), b)
+
+# Mean squared error
+cost = tf.pow(pred - Y, 2)
+# Gradient descent
+#  Note, minimize() knows to modify W and b because Variable objects are trainable=True by default
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+# Initialize the variables (i.e. assign their default value)
+init = tf.global_variables_initializer()
+zipped = zip(train_X, train_Y)
+
+# Start training
+with tf.Session() as sess:
+    # Run the initializer
+    sess.run(init)
+
+    # Fit all training data
+    for epoch in range(training_epochs):
+        for (x, y) in zipped:
+            sess.run(optimizer, feed_dict={X: x, Y: y})
+
+        # Display logs per epoch step
+        if (epoch + 1) % display_step == 0:
+            c = sess.run(tf.reduce_sum(cost), feed_dict={X: train_X, Y: train_Y})
+            print("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(c),
+                  "m=", sess.run(m), "b=", sess.run(b))
+
+    print("Optimization Finished!")
+    training_cost = sess.run(tf.reduce_sum(cost), feed_dict={X: train_X, Y: train_Y})
+    print("Training cost=", training_cost, "m=", sess.run(m), "b=", sess.run(b), '\n')
+
+    # Graphic display
+    plt.plot(train_X, train_Y, 'ro', label='Original data')
+    plt.plot(train_X, sess.run(m) * train_X + sess.run(b), label='Fitted line')
+    plt.legend()
+    plt.show()
